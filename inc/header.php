ea2842471a694b397747fd8623c8537beaab8afc
Felipe <felipessouza@outlook.com>
2020-06-03 18:49:50 -0300

    first

---
+++ inc/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>DR RAFAEL</title>
+
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,800&display=swap">
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/modal-video/css/modal-video.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/master.css">
+
+</head>
+<body>
+
+  <header id="header">
+    <div class="container">
+      <div class="row">
+
+        </div>
+      </div>
+    </header>
---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready( () => {
+  $(".burger").click(() => {
+    $(".menu li").fadeToggle(2000);
+    $(".menu").animate({width: "toggle"}, 900);
+  });
+});
