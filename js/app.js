30f72a62ac3a597a6775f9e55ec3a4dd8700cc3a
Felipe <felipessouza@outlook.com>
2020-06-04 00:12:34 -0300

    menu mobile

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $(document).ready( () => {
+
   $(".burger").click(() => {
     $(".menu").animate({
       width: ["toggle", "swing"],
       opacity: "toggle"
-    }, 900);
+    }, 1000);
   });
+
+
+  $(window).scroll(() => {
+    $(".burger").addClass("shadow", 1000);
+  });
+
 });
